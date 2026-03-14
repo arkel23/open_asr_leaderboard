996854deaddfbabc3b6df9508ff2a98b28fdca1c
jn <jocelyn.zaruma@proton.me>
2026-03-14 13:33:18 +0100

    add comments

--- custom_dataset_utils/dataset_loader.py
+++ custom_dataset_utils/dataset_loader.py
@@ -1,18 +1,7 @@
-from datasets import load_dataset, Audio, Dataset
+from datasets import load_dataset, Audio, IterableDataset
 
-
-
-"""
-
-Currently this works for McGuill African Celtic Shared Task Dataset
-however language columns can vary: 
-- ex - lang_id(with iso-2 iso-3 or iso-2_locale iso_3_locale), language.
-- we can build on this.
-
-"""
-## 
-
-def load_filtered_dataset(dataset, config, split, language=None, streaming=True, token=True):
+## Adapt to the speechbrain/LoquaciousSet DS Transformers/run_eval_ml.py
+def load_filtered_dataset(dataset, config, split, streaming=True, token=True):
     ds = load_dataset(dataset, config, split=split, streaming=streaming, token=token)
 
     if "wav" in ds.column_names:
@@ -25,9 +14,7 @@ def load_filtered_dataset(dataset, config, split, language=None, streaming=True,
     return ds
 
 
-
-from datasets import load_dataset, Audio, IterableDataset
-
+## GET THE NESTED FLEURS DS (contains the audio) Voxtral/run_eval_ml.py
 def load_filtered_data_slu_fleurs_audios(dataset, config, split, streaming=True, token=True):
     slu_ds = load_dataset(dataset, config, split=split, streaming=streaming, token=token)
 
